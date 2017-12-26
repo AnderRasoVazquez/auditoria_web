88c01ac3b9436201baf8f0d1c6e75bef232b7736
DavidPerezGomez <davidperezgo@gmail.com>
2017-12-26 15:54:03 +0100

    forzado el uso de utf8 en la BD

--- serv.php
+++ serv.php
@@ -2,4 +2,5 @@
 	$conexion = mysqli_connect("localhost","Xdperez067","AVmu8sW4r") or die("Fallo en el establecimiento de la conexión");
 	// mysqli_select_db($conexion,"Xdperez067_db_auditoria_sgssi")
 	mysqli_select_db($conexion,"Xdperez067_db_auditoria_sgssi") or die ("Error en la selección de la base de datos");
+    $connection->set_charset("utf8");
 ?>
