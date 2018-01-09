7390b060ccceb688b39f7cff8a230e8a059d547a
Ander Raso Vázquez <anderraso@gmail.com>
2018-01-09 19:15:08 +0100

    implementada la parametización del insert player

--- insertplayer.php
+++ insertplayer.php
@@ -10,30 +10,39 @@ if(!isset($_SESSION['usuario'])) {
     session_unset();
     session_destroy();
     echo 'Sesión cerrada por inactividad.';
-	echo '<script> window.location="inicio.php"; </script>';
+    echo '<script> window.location="inicio.php"; </script>';
 }
 $_SESSION['tiempo'] = time();
-	if(isset($_POST['submit'])){
-	$name = $_POST['nombrej'];
-	$dateplay = $_POST['fechaj'];
-	$team = $_POST['equipo'];
-	$nation = $_POST['nac'];
-	//comprobamos que el jugador no existe/el select hay que ponerle bien los nombres/
-	$result = mysqli_query($conexion, "SELECT * FROM Jugadores WHERE Nombre='$name'");
-	$data = mysqli_num_rows($result);
-	if(($data)>0){
-		echo '<script> alert("El jugador ya está creado.");</script>';
-	}
-	if (($data)==0){
-		//hay que poner bien los nombres del INSERT
-		$query = mysqli_query($conexion, "INSERT INTO Jugadores(Nombre, Nacionalidad, FechaNacimiento, NombreEquipo) VALUES ('$name', '$nation', '$dateplay', '$team')");
-		if($query){
-			echo '<script> alert("¡Has añadido el jugador correctamente!");</script>';
-		}else{
-			echo "Error....!!";
-		}
-	}
-	}
-	mysqli_close($connection);
-	echo '<script> window.location="panel.php"; </script>';
+
+if(isset($_POST['submit'])){
+	$sql2 = "SELECT * FROM Jugadores WHERE Nombre=?";
+	$sent2 = $conexion->prepare($sql2);
+	$sent2->bind_param("s", $name);
+    $name = $_POST['nombrej'];
+	$sent2->execute();
+	$result2 = $sent2->get_result();
+	$data2 = mysqli_num_rows($result2) > 0;
+	$sent2->close();
+	if ($data2) {
+        echo '<script> alert("El jugador ya está creado.");</script>';
+	} else {
+        $sql2 = "INSERT INTO Jugadores(Nombre, Nacionalidad, FechaNacimiento, NombreEquipo) VALUES (?, ?, ?, ?)";
+        $sent2 = $conexion->prepare($sql2);
+        $sent2->bind_param("ssss", $name, $nation, $dateplay, $team);
+
+        $name = $_POST['nombrej'];
+        $dateplay = $_POST['fechaj'];
+        $team = $_POST['equipo'];
+        $nation = $_POST['nac'];
+
+        if($sent2->execute()){
+            echo '<script> alert("¡Has añadido el jugador correctamente!");</script>';
+        }else{
+            echo "Error....!!";
+        }
+    }
+}
+
+mysqli_close($conexion);
+echo '<script> window.location="panel.php"; </script>';
 ?>
