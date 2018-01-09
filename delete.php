839b0350e977a120b011c7419d9a8576adc2727e
Ander Raso Vázquez <anderraso@gmail.com>
2018-01-09 17:29:53 +0100

    parametización implementada en delete.php

--- delete.php
+++ delete.php
@@ -12,12 +12,22 @@ if(!isset($_SESSION['usuario'])) {
 	echo '<script> window.location="inicio.php"; </script>';
 }
 $_SESSION['tiempo'] = time();
+
+include 'serv.php';
+
+$sql = "DELETE FROM Jugadores WHERE ID = ?";
+# sentencia
+$sent = $conexion->prepare($sql);
+if($sent === false) {
+    trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conexion->errno . ' ' . $conexion->error, E_USER_ERROR);
+}
+
+$sent->bind_param("i", $id);
 $id = $_GET['id'];
-$conexion = mysqli_connect("localhost","Xdperez067","AVmu8sW4r") or die("Fallo en el establecimiento de la conexión");
-mysqli_select_db($conexion,"Xdperez067_db_auditoria_sgssi") or die ("Error en la selección de la base de datos");
-$query = "DELETE FROM Jugadores WHERE ID = '$id'";
-mysqli_query($conexion, $query) or die('Database error!');
+
+$sent->execute();
 echo "<script> alert('Elemento borrado');</script>";
-mysqli_close($connection);
+mysqli_close($conexion);
 echo '<script> window.location="panel.php"; </script>';
+
 ?>
