a3d3c3eb6fd328b14e66ded2020eca141e3c2d64
Ander Raso Vázquez <anderraso@gmail.com>
2018-01-09 19:01:51 +0100

    comentada la sesion porque si está activada al introducir los datos no le da tiempo a cotejarlos por tardar mas de un minuto, e implementada la parametizacion

--- serv.php
+++ serv.php
@@ -3,4 +3,33 @@
     // mysqli_select_db($conexion,"Xdperez067_db_auditoria_sgssi")
     mysqli_select_db($conexion,"Xdperez067_db_auditoria_sgssi") or die ("Error en la selección de la base de datos");
     $conexion->set_charset("utf8");
+
+    // $sql = "SELECT Nombre FROM Jugadores where ID= ?";
+    // # sentencia
+    // $sent = $conexion->prepare($sql);
+    // if($sent === false) {
+    //     trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conexion->errno . ' ' . $conexion->error, E_USER_ERROR);
+    // }
+    //
+    // $sent->bind_param("i", $super_id);
+    // $super_id = $_GET["id"];
+    //
+    // $sent->execute();
+    // $sent->bind_result($name);
+    //
+    // while ($row = $sent->fetch()) {
+    //     echo($name);
+    //     echo("   ");
+    // } ;
+    //
+	// if ($sent->num_rows > 0) {
+    //     echo ("tomaaaaaa");
+    // } else {
+    //     echo ("niggggga");
+    // }
+    //
+    // mysqli_close($conexion);
+
+
+
 ?>
