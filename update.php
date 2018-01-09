d72fe6288780f50537192d8698f41f6ca803a0b5
Ander Raso Vázquez <anderraso@gmail.com>
2018-01-09 19:58:04 +0100

    implementada parametización

--- update.php
+++ update.php
@@ -15,20 +15,23 @@ if(!isset($_SESSION['usuario'])) {
 $_SESSION['tiempo'] = time();
 
 if(isset($_POST['submit'])){
+	$sql2 = "UPDATE Jugadores SET Nombre=?, Nacionalidad=?,FechaNacimiento=?, NombreEquipo=? WHERE ID=?";
+    $sent2 = $conexion->prepare($sql2);
+    $sent2->bind_param("ssssi", $nombre, $nac, $fecha, $equipo, $id);
+
 	$id = $_POST['id'];
 	$nombre = $_POST['nombre'];
 	$nac = $_POST['nac'];
 	$fecha = $_POST['fecha'];
 	$equipo = $_POST['equipo'];
-	echo "<script> alert('voy a modificar: $id');</script>";
-	$result = mysqli_query($conexion, "UPDATE Jugadores SET Nombre = '$nombre', Nacionalidad = '$nac',FechaNacimiento =  '$fecha', NombreEquipo = '$equipo' WHERE  ID ='$id'");
-	if($result){
-			echo '<script> alert("¡Has actualizado el jugador correctamente!");</script>';
-		}else{
-			echo "Error....!!";
-		}
-	}
-	mysqli_close($conexion);
 
-	echo '<script> window.location="panel.php"; </script>';;
+    if($sent2->execute()){
+		echo '<script> alert("¡Has actualizado el jugador correctamente!");</script>';
+    }else{
+        echo "Error....!!";
+    }
+
+	mysqli_close($conexion);
+	echo '<script> window.location="panel.php"; </script>';
+}
 ?>
