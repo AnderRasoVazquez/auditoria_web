be3d263f5dc6abbddad678a6fb923902f20e653d
DavidPerezGomez <davidperezgo@gmail.com>
2018-01-11 23:31:44 +0100

    pasado el comenatio a html para que pueda ser vsible

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 echo '<script> window.location="inicio.php"; </script>';
-// sabes que estaría guay? tener un poco de idea de como está configurado
-// el servidor al que tenemos que subir las contraseñas para no tener
-// que ir probando linea a linea que es lo que falla porque el servidor
-// utiliza un php anticuado o porque no tiene activado el driver mysqlInd
-// https://stackoverflow.com/questions/8321096/call-to-undefined-method-mysqli-stmtget-result/11135291#11135291
-// salve stackoverflow
-// mucho "las secuencias sql mejor con atributos parametrizados..." pero luego
-// resulta que la forma mas accesible para hacerlo (->get_result()) no funciona
-// L
-// O
-// L
 ?>
+<!--sabes que estaría guay? tener un poco de idea de como está configurado
+el servidor al que tenemos que subir las contraseñas para no tener
+que ir probando linea a linea que es lo que falla porque el servidor
+utiliza un php anticuado o porque no tiene activado el driver mysqlInd
+https://stackoverflow.com/questions/8321096/call-to-undefined-method-mysqli-stmtget-result/11135291#11135291
+salve stackoverflow
+mucho "las secuencias sql mejor con atributos parametrizados..." pero luego
+resulta que la forma mas accesible para hacerlo (->get_result()) no funciona
+L
+O
+L/-->
